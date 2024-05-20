7c7f9e069b125636e02d28269f3b42ae634498ef
Fabian Seiler <fabian.seiler@outlook.de>
2024-05-20 16:02:52 +0200

    Added animations

---
+++ MasterThesisBRP/Assets/InstructionStep.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionStep : MonoBehaviour
+{
+    public string instructionText;
+    public Animation animation;
+    public int stepNumber;
+}
